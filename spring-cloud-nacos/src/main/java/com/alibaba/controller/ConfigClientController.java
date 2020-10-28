eea7c23681a334f03cf789967be01b9066c23a77
jidening <dening1644@163.com>
2020-10-28 11:43:17 +0800

    spring alibaba nacos config code submit

---
+++ spring-cloud-nacos/src/main/java/com/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jidn
+ * @Date 2020/10/28
+ */
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String hello() {
+        return this.profile;
+    }
+}
